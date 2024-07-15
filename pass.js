fe3a7a537784984101e881c70929c035cd26a98b
SatheeshKumar42 <160118761+SatheeshKumar42@users.noreply.github.com>
2024-07-15 15:41:29 +0530

    first commit

---
+++ pass.js
@@ -0,0 +1,28 @@
+const passwordBox=document.getElementById('password')
+const length=8;
+
+const uppercase='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowercase='abcdefghijklmnopqrstuvwxyz'
+const number='0123456789'
+const symbol="*@#+%~,;:.{}<>[]()\/"
+
+const allchars=uppercase+lowercase+number+symbol
+
+
+function createpassword(){
+    let password='';
+    password += uppercase[Math.floor(Math.random()*uppercase.length)]
+    password += lowercase[Math.floor(Math.random()*lowercase.length)]
+    password += number[Math.floor(Math.random()*number.length)]
+    password += symbol[Math.floor(Math.random()*symbol.length)]
+    
+    while(length > password.length){
+        password += allchars[Math.floor(Math.random()*allchars.length)]
+    }
+    passwordBox.value=password
+}
+
+function copypass(){
+    passwordBox.select();
+document.execCommand('copy')
+}
